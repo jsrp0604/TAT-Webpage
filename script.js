68bc5c64c186cc50d898876f03442f31a0715a09
jsramirez <jsramirez@uees.edu.ec>
2024-11-12 17:14:06 -0500

    1.1 Version of the page / Added download links
    Added download links for "ejemplos" buttons in "casos" sections

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const btnPlantillas = document.getElementById("btnPlantillas");
-const btnManual = document.getElementById("btnManual")
+const btnManual = document.getElementById("btnManual");
+const btnFicha = document.getElementById("btnFicha");
+const btnEjNiño = document.getElementById("btnEjNiño");
+const btnEjMujer = document.getElementById("btnEjMujer");
+const btnEjHombre = document.getElementById("btnEjHombre");
 
 const filePlantillas = "laminas-tat.pdf";
-const fileManual = "TAT-Manual-de-aplicacion.pdf"; 
+const fileManual = "TAT-Manual-de-aplicacion.pdf";
+const fileFicha = "ficha-de-calificación-tat.xlsx";
+const fileEjNiño = "adolescente-de-13-años.pdf";
+const fileEjMujer = "caso-mujer-20-años.pdf";
+const fileEjHombre = "caso-hombre-de-31-años.pdf";
 
 addDownloadLink(btnPlantillas, filePlantillas);
 addDownloadLink(btnManual, fileManual);
+addDownloadLink(btnFicha, fileFicha);
+addDownloadLink(btnEjNiño, fileEjNiño);
+addDownloadLink(btnEjMujer, fileEjMujer);
+addDownloadLink(btnEjHombre, fileEjHombre);
 
 function addDownloadLink(btn, file)   {
     btn.addEventListener("click", () => {
